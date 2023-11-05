ff143ea7a34c3032c0a278b90da325b02abe811c
Graey <72920718+Aceesuu@users.noreply.github.com>
2023-11-05 15:26:22 +0800

    Update order_crud.php

--- order_crud.php
+++ order_crud.php
@@ -1,24 +1,6 @@
 <?php
-session_start(); // Start the session
-
-if (!isset($_SESSION['user_id'])) {
-    // Redirect to index.php or login page if user is not logged in
-    header("Location: index.php"); // Update with your login page URL
-    exit();
-}
 
 include("mysql_connect.php");
-$user_id = $_SESSION['user_id'];
-$query = "SELECT * FROM tb_user WHERE user_id = '$user_id'";
-$user_result = mysqli_query($conn, $query);
-
-if ($user_result && mysqli_num_rows($user_result) > 0) {
-    $user_data = mysqli_fetch_assoc($user_result);
-    // Now you can use $user_data to access user information
-} else {
-    // Handle the case where user data couldn't be retrieved
-    $error_message = "Error: Unable to retrieve user data.";
-}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $orderId = $_POST['order_id'];
@@ -46,3 +28,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 } else {
     echo "Invalid request method.";
 }
+?>
